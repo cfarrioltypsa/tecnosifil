463746132391a1b435652b48b362bce2c6d8aa5a
cfarrioltypsa <107852463+cfarrioltypsa@users.noreply.github.com>
2023-03-13 08:39:40 +0100

    Update component.model.js

--- src/api/component/component.model.js
+++ src/api/component/component.model.js
@@ -8,8 +8,9 @@ const componentSchema = new mongoose.Schema({
   component_name: {
     type: String,
   },
-  isChecked: 
-  type: Boolean
+  isChecked: {
+    type: Boolean
+  }
 });
 
 const Component = mongoose.model('Component', componentSchema);
